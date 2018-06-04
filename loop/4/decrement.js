f7b9a401f060eb66d30ef3aaf62e08bf0c8be2a6
jakdev047 <jakfreelencer047@gmail.com>
2018-06-05 00:38:15 +0600

    increment & decerment

---
+++ loop/4/decrement.js
@@ -0,0 +1,22 @@
+/*
+    post-decrement
+    x = y--
+    x=y;
+    y= y-1;
+*/
+
+var y = 4;
+var x = y--
+    console.log(x, y);
+
+console.log('\n reset \n');
+
+/*
+    pre-decrement
+    x = --y
+    x=1-y;
+    y=1-y;
+*/
+var y = 3;
+var x = --y;
+console.log(x, y); 
---
+++ loop/4/increment.js
@@ -0,0 +1,23 @@
+
+/*
+    post-increment
+    x = y++
+    x=y;
+    y= y+1;
+*/
+     
+    var  y = 4;
+    var x = y++
+    console.log(x,y);
+
+    console.log('\n reset \n');
+
+/*
+    pre-increment
+    x = ++y
+    x=1+y;
+    y=1+y;
+*/
+    var y = 3;
+    var x = ++y;
+    console.log(x, y);
